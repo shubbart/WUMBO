f9bb774472eaf08b90d2f0e12c4c5007f72d05a1
Shawn Hubbart <s179128@students.aie.edu.au>
2017-11-02 14:16:35 -0700

    Created a bounce trigger, cannon, and cannon trigger for future use

---
+++ Assets/Scripts/Shawn Scripts/TriggerEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public GameObject targetLocation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            targetLocation.GetComponent<Shoot>().Fire();
+    }
+
+}

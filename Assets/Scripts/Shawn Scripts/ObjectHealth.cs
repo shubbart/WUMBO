667d1b2ccef637e4081f7b3f1d9eced593902c79
Shawn Hubbart <s179128@students.aie.edu.au>
2017-11-03 11:44:24 -0700

    Created a bubble particle effect, snow particle effect, flamethrower weapon effect, frost weapon effect, and implemented scripts that allow the weapons to damage objects depending on weapon type

---
+++ Assets/Scripts/Shawn Scripts/ObjectHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHealth : MonoBehaviour
+{
+    public int health;
+
+	void Update ()
+    {
+        if (health <= 0)
+            Die();
+	}
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}

d1971481cf45905a7caad08108a8841aae9a2715
Shawn Hubbart <s179128@students.aie.edu.au>
2017-11-08 15:16:31 -0800

    Added power orb. Pick up code still needed.

---
+++ Assets/Scripts/Shawn Scripts/PowerOrbController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerOrbController : MonoBehaviour
+{
+    //public float rotationTimer;
+    //float timerReset;
+    public Vector3 minRotationSpeed;
+    public Vector3 maxRotationSpeed;
+    Vector3 randomRotation;
+
+    void Start ()
+    {
+        //timerReset = rotationTimer;
+        randomRotation = new Vector3(Random.Range(minRotationSpeed.x, maxRotationSpeed.x),
+                            Random.Range(minRotationSpeed.y, maxRotationSpeed.y),
+                            Random.Range(minRotationSpeed.z, maxRotationSpeed.z));
+    }
+	
+	void Update ()
+    {
+
+        //rotationTimer -= Time.deltaTime;
+
+        //if (rotationTimer <= 0)
+        //{
+        //    randomRotation = new Vector3(Random.Range(minRotationSpeed.x, maxRotationSpeed.x),
+        //                                Random.Range(minRotationSpeed.y, maxRotationSpeed.y),
+        //                                Random.Range(minRotationSpeed.z, maxRotationSpeed.z));
+        //    rotationTimer = timerReset;
+        //}
+        transform.Rotate(randomRotation);
+    }
+}

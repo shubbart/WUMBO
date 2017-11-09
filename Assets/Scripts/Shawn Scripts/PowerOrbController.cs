571ce11910623e07d074d3c1f7592a3c8aa7e502
Shawn Hubbart <s179128@students.aie.edu.au>
2017-11-08 17:58:56 -0800

    Added a 'Power Orb' prefab and an effect for the gravity beam

--- Assets/Scripts/Shawn Scripts/PowerOrbController.cs
+++ Assets/Scripts/Shawn Scripts/PowerOrbController.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class PowerOrbController : MonoBehaviour
 {
-    //public float rotationTimer;
-    //float timerReset;
     public Vector3 minRotationSpeed;
     public Vector3 maxRotationSpeed;
+    public GameObject gravityAura;
     Vector3 randomRotation;
 
     void Start ()
     {
-        //timerReset = rotationTimer;
         randomRotation = new Vector3(Random.Range(minRotationSpeed.x, maxRotationSpeed.x),
                             Random.Range(minRotationSpeed.y, maxRotationSpeed.y),
                             Random.Range(minRotationSpeed.z, maxRotationSpeed.z));
@@ -20,16 +18,14 @@ public class PowerOrbController : MonoBehaviour
 	
 	void Update ()
     {
-
-        //rotationTimer -= Time.deltaTime;
-
-        //if (rotationTimer <= 0)
-        //{
-        //    randomRotation = new Vector3(Random.Range(minRotationSpeed.x, maxRotationSpeed.x),
-        //                                Random.Range(minRotationSpeed.y, maxRotationSpeed.y),
-        //                                Random.Range(minRotationSpeed.z, maxRotationSpeed.z));
-        //    rotationTimer = timerReset;
-        //}
         transform.Rotate(randomRotation);
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if(other.tag == "Gravitygun")
+        {
+            gravityAura.SetActive(true);
+        }
     }
 }

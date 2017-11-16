76a500ff3a1397a3878289d643794dac47fa1885
Shawn Hubbart <s179128@students.aie.edu.au>
2017-11-15 17:41:05 -0800

    Added a console UI that - as of now -  includes only the damage recieved in percentile

---
+++ Assets/Scripts/Shawn Scripts/DamageText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageText : MonoBehaviour
+{
+    Text damage;
+    GameObject player;
+    PlayerManager pManager;
+
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pManager = player.GetComponent<PlayerManager>();
+        damage = GetComponent<Text>();
+
+        damage.text = "Damage: " + (100 - pManager.health) + "%";
+	}
+	
+	public void SetDamage ()
+    {
+        damage.text = "Damage: " + (100 - pManager.health) + "%";
+    }
+}

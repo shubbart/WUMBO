01f41ffb577355029b61f845d01b805b8ca97d5c
keilaloia <keil.aloia@students.aie.edu.au>
2017-11-16 17:42:27 -0800

    fuck vr

---
+++ Assets/RemoveXRotation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveXRotation : MonoBehaviour
+{	
+	// Update is called once per frame
+	void Update ()
+    { transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z); }
+}
